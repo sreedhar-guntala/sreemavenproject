ae6d73df4fba5e58bc668cd460c8ab79f6916be6
sreedhar-guntala <sreenaths29@gmail.com>
2020-03-16 15:52:04 +0000

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!.. Poll SCM Testing..!..Again" );
+        System.out.println( "Hello World!.. Poll SCM Testing..!..Again..Again..!!" );
     }
 }
